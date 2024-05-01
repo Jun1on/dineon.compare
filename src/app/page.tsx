ce60f5c829e5e36b201c02857f9a97a50895d3b5
jun1on <jczhang001@gmail.com>
2024-05-01 01:53:04 -0500

    create landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { Spotlight } from "@/components/ui/spotlight";
+import { ComboboxDemo } from "./select";
+
+export default function BackgroundGradientAnimationDemo() {
+  const interactiveRef = useRef<HTMLDivElement>(null);
+  const [gradientX, setGradientX] = useState(50);
+  const [gradientY, setGradientY] = useState(50);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (interactiveRef.current) {
+      const rect = interactiveRef.current.getBoundingClientRect();
+      // Calculate gradient position as a percentage of the bounding box size
+      const x = ((event.clientX - rect.left) / rect.width) * 100;
+      const y = ((event.clientY - rect.top) / rect.height) * 100;
+      setGradientX(x);
+      setGradientY(y);
+    }
+  };
+
+  return (
+    <div onMouseMove={handleMouseMove} className="h-screen w-full bg-black bg-grid-white/[0.2] relative flex items-center justify-center antialiased overflow-hidden">
+      <div
+        ref={interactiveRef}
+        className={`absolute inset-0 bg-blue`/* [mask-image:radial-gradient(ellipse_at_3.76%_6.29%,transparent_0%,black)]*/}
+        style={{
+          background: `radial-gradient(circle at ${gradientX}% ${gradientY}%, transparent, black)`,
+        }}
+      ></div>
+      
+      <div className="flex flex-col items-center p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center mb-10">
+          <span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-500 bg-opacity-50">
+            Compare dining options.
+          </span>
+        </h1>
+        <ComboboxDemo />
+        <div className="m-12" />
+      </div>
+    </div>
+  );
+}
---
+++ src/app/select.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function ComboboxDemo() {
+  const [open, setOpen] = React.useState(false);
+  const [schools, setSchools] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.dineoncampus.com/v1/sites/public")
+      .then((res) => res.json())
+      .then((data) => {
+        setSchools(
+          data.sites.map((school: { name: string; slug: string }) => ({
+            name: school.name,
+            slug: school.slug,
+          }))
+        );
+      })
+      .catch((error) => {
+        console.error("Failed to fetch schools", error);
+      });
+  }, []);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger
+        asChild
+        onMouseEnter={() => setOpen(true)}
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div>
+          <HoverBorderGradient className="w-[300px]" open={open}>
+            <span>Select your school</span>
+          </HoverBorderGradient>
+        </div>
+      </PopoverTrigger>
+      <PopoverContent className="w-[300px] p-0">
+        <Command>
+          <CommandInput placeholder="Search school" />
+          <CommandEmpty>
+            {schools.length ? "School not found" : "Loading..."}
+          </CommandEmpty>
+          <CommandGroup>
+            <CommandList>
+              {schools.map((option: { name: string; slug: string }) => (
+                <CommandItem
+                  style={{ cursor: "pointer" }}
+                  key={option.slug}
+                  value={option.slug}
+                  onSelect={() => {
+                    window.location.href = "/" + option.slug;
+                  }}
+                >
+                  {option.name}
+                </CommandItem>
+              ))}
+            </CommandList>
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
